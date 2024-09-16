c6a5a60048a3f1497d90ce79c242114517f326ab
Kgaotlhaelwe <Kgaotlhaelwe@gmail.com>
2024-09-16 10:54:51 +0100

    test

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,10 +4,10 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCQ-9ClK7s80XvVGUak-TlBA2UunLm7d1o",
-  authDomain: "limpopo-firebase-class.firebaseapp.com",
-  projectId: "limpopo-firebase-class",
-  storageBucket: "limpopo-firebase-class.appspot.com",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
   messagingSenderId: "734557935548",
   appId: "1:734557935548:web:702cd0572b5ad437eefdb9",
   measurementId: "G-194G8CP7PP"
